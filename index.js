f9e71bd2c4fc59c63396ef19f191fe42de945676
AnumShabbir032 <AnumShabbir032@gmail.com>
2021-09-28 17:13:55 +0500

    Guess the game

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const scrambleWords = (arr) => {
      for(var i =arr.length-1; i>0 ; i--){
          let finalWord =arr[i];
          let a = Math.floor(Math.random()* (i+1));
+        
          arr[i] = arr[a];
          arr[a] = finalWord; 
      }
